47fbb063d716c3b8f8ceeda8b1a86fa422066e24
iuresti <iuresti@skytouchtechnology.com>
2018-06-16 12:48:39 -0500

    initial commit

---
+++ src/main/java/com/iuresti/test/herokuci/HerokuCiApplication.java
@@ -0,0 +1,12 @@
+package com.iuresti.test.herokuci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerokuCiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HerokuCiApplication.class, args);
+    }
+}
